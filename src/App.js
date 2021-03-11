5b445a9968120b9a2f8971008a4fc297e912fa8e
Jake Barber <jabot999@gmail.com>
2021-03-11 16:31:34 -0500

    copied contents of App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; //node modules/bootstrap
+import Particles from "react-particles-js";
+import Navbar from './components/Navbar'
+import Header from "./components/Header"
+import About from "./components/About"
+import Portfolio from "./components/Portfolio"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles 
+        className="particles-canvas"
+        params={{
+          "particles": {
+            "number": {
+              "value": 15, 
+              "density": {
+                "enable": true, 
+                "value_area": 900
+              }
+            },//number
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 5,
+                "color": "#FF9900" //bitcoin orange hex
+              }
+            },//shape
+          },//particles
+          "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                  }
+              }
+          }
+        }}
+z
+      />
+      <Navbar className="sticky-=nav" />
+      <Header />
+      <About />
+      <Portfolio />
+      
+    </>  
   );
 }
 
