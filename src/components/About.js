7ca88f550a79947ade5bed1ed0e628492969fb83
Jake Barber <jabot999@gmail.com>
2021-03-19 18:17:27 -0400

    implemented Gwen's orders

--- src/components/About.js
+++ src/components/About.js
@@ -8,44 +8,47 @@ const About = () => {
         <div className="about" id="about" >
             <br /><br /><br />
 
-            
-            <Zoom delay={200}>
-            <div className="about-words">              
-                <h2>About Me</h2>
-            </div>
-            <div className="about-words">              
-                <p className="about-info" >
-                    My name is <a className="text-link" href="https://linkedin.com/in/jakebarber1996" target="_blank" rel="noreferrer">Jake Barber</a> and I am an aspiring software developer, hoping to specialize in blockchain development on Ethereum.
-                </p>
-            </div>
-            <div className="about-words">              
-                <p className="about-info">
-                    I have a Bachelors degree (Graduated 2018) from <a className="text-link" href={raddy} target="_blank" rel="noreferrer">Radford University</a> in Computer Science and Technology, with a concentration in Database Design, as well as an Undergraduate Information Security Certificate recognized by the NSA.  
-                </p>
-            </div>
-            </Zoom>         
 
             <Zoom delay={200}>
-            <div className="links">
-                <div>
-                    <h1 className="about-header">Contact Information</h1>
+            <h2>About Me</h2>
+                <div className="about-words">       
+                    <p className="about-info" >
+                        My name is <a className="text-link" href="https://linkedin.com/in/jakebarber1996" target="_blank" rel="noreferrer">Jake Barber</a> and I am an aspiring software developer, hoping to specialize in blockchain development on Ethereum.
+                    </p>
+                    <p className="about-info" >
+                        I have a rich educational background in software development, data management, and information security. In addition, I have extensive professional experience implementing those core skills as an IT contractor for the United States Federal Government. As I continued to rapidly take on more responsibility within the company, I consistently demonstrated my ambition and desire for excellence in my work, my aptitude to learn at an accelerated pace, and my enthusiasm to communicate that knowledge to my peers.
+                    </p>
+                    <p>
+                        At this time, I am eager to specialize further in the field of software development and I am excited to do so in the realm of blockchain technology.
+                    </p>       
+                    <p className="about-info" id="contact-info">
+                        I graduated from <a className="text-link" href={raddy} target="_blank" rel="noreferrer">Radford University</a> in 2018 with a Bachelor's degree in Computer Science and Technology, with a concentration in Database Design, as well as an Undergraduate Information Security Certificate recognized by the NSA.
+                </p>
+                </div>
+            </Zoom>
+            <br />
+            <Zoom delay={200}>
+                <div className="links" >
                     
+                    <div>
+                        <h1 className="about-header">Contact Information</h1>
+
                         <p>Jake Barber</p>
                         <p>Mobile: (703) 635-8884</p>
                         <p>jabot999@gmail.com</p>
-                        <p><a className="text-link" href="https://www.jake-barber.com/" target="_blank" rel="noreferrer">Personal Website</a></p>                    
-                    
-                    <div className="resumeButton">
-                    <a href="https://drive.google.com/file/d/1XqDIdbJ-AOnlPS5ROrAxYglURUrxmAvU/view?usp=sharing" target="_blank" rel="noreferrer" className="contact">RÉsumÉ</a>
-                    </div>
-                </div>       
-            </div>
-            </Zoom>
-            
+                        <p><a className="text-link" href="https://www.linkedin.com/in/jakebarber1996/" target="_blank" rel="noreferrer">My LinkedIn</a></p>
 
-            
+                        <div className="resumeButton">
+                            <a href="https://drive.google.com/file/d/1XqDIdbJ-AOnlPS5ROrAxYglURUrxmAvU/view?usp=sharing" target="_blank" rel="noreferrer" className="contact">RÉsumÉ</a>
+                        </div>
+                    </div>
+                </div>
+            </Zoom>
+
+
+
         </div>
-        
+
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className="component-wraper" id="header">
             <div className="main-info">
+                <br /><br /><br /><br /><br /><br /><br /><br />
                 <h1>Welcome to my Personal Website!</h1>
                 <p>This website was built by me from scratch using React JavaScript, and will always be a work in progress...</p>
                 <p>It may or may not scale to mobile device screen sizes at this time....</p>
@@ -16,7 +17,7 @@ const Header = () => {
                     loop                
                 />
 
-                <a href="#about" className="contact"> Contact Me</a>
+                <a href="#contact-info" className="contact"> Contact Me</a>
             </div>            
         </div>
     )
