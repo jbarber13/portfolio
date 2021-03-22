5bb91bf62dfcc9e4ee09e3ea84189f971a9f6096
Jake Barber <jabot999@gmail.com>
2021-03-22 15:43:50 -0400

    re-ordered About <p>

--- src/components/About.js
+++ src/components/About.js
@@ -15,15 +15,16 @@ const About = () => {
                     <p className="about-info" >
                         My name is <a className="text-link" href="https://linkedin.com/in/jakebarber1996" target="_blank" rel="noreferrer">Jake Barber</a> and I am an aspiring software developer, hoping to specialize in blockchain development on Ethereum.
                     </p>
+                    <p className="about-info" id="contact-info">
+                        I graduated from <a className="text-link" href={raddy} target="_blank" rel="noreferrer">Radford University</a> in 2018 with a Bachelor of Science Degree in Computer Science and Technology, with a concentration in Database Design, as well as an Undergraduate Information Security Certificate recognized by the NSA.
+                    </p>
                     <p className="about-info" >
                         I have a rich educational background in software development, data management, and information security. In addition, I have extensive professional experience implementing those core skills as an IT contractor for the United States Federal Government. As I continued to rapidly take on more responsibility within the company, I consistently demonstrated my ambition and desire for excellence in my work, my aptitude to learn at an accelerated pace, and my enthusiasm to communicate that knowledge to my peers.
                     </p>
                     <p>
                         At this time, I am eager to specialize further in the field of software development and I am excited to do so in the realm of blockchain technology.
                     </p>       
-                    <p className="about-info" id="contact-info">
-                        I graduated from <a className="text-link" href={raddy} target="_blank" rel="noreferrer">Radford University</a> in 2018 with a Bachelor's degree in Computer Science and Technology, with a concentration in Database Design, as well as an Undergraduate Information Security Certificate recognized by the NSA.
-                </p>
+                    
                 </div>
             </Zoom>
             <br />
