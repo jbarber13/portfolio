730140facc1aeb8498ae467dcce0bd5872ada4db
Jake Barber <jabot999@gmail.com>
2021-03-23 15:22:08 -0400

    fixed header text and some more mobile scaling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,21 @@ const Header = () => {
         <div className="component-wraper" id="header">
             <div className="main-info">
                 <br /><br /><br /><br /><br /><br /><br /><br />
-                <h1>Welcome to my Personal Website!</h1>
-                <p>This website was built by me from scratch using React JavaScript, and will always be a work in progress...</p>
-                <p>It may or may not scale to mobile device screen sizes at this time....</p>
+                <h1>Blockchain is the future!</h1>
+                <br />
+                <p>My name is Jake Barber, and I am an expierenced IT proffesional and software developer, specializing in blockchain technology.</p>
+                <p>Scroll down to learn more!</p>
+                <div className="typed">
                 <Typed 
                     className="typed-text"
                     strings={["Blockchain", "Ethereum", "Solidity", "Web3", "React", "Redux", "Truffle", "Ganache"]}
                     typeSpeed={80}
                     backSpeed={100}
-                    loop                
+                    loop    
+                    shuffle            
                 />
+                </div>
+                
 
                 <a href="#contact-info" className="contact"> Contact Me</a>
             </div>            
