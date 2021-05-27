11e8612e31f486949ea70b8bc7323083cadf3357
Jake Barber <jabot999@gmail.com>
2021-05-27 15:30:42 -0400

    fixed About words

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,11 @@ const raddy = "https://www.radford.edu/content/radfordcore/home.html"
 const About = () => {
     return (
         <div className="about" id="about" >
-            <br /><br /><br />
+            <br /><br />
 
 
             <Zoom delay={200}>
-            <h2>About Me</h2>
+            <h2 className="pt-4">About Me</h2>
                 <div className="about-words">       
                     <p className="about-info" >
                         My name is <a className="text-link" href="https://linkedin.com/in/jakebarber1996" target="_blank" rel="noreferrer">Jake Barber</a> and I am an experienced Software Developer and IT Professional, specializing in blockchain technology.
@@ -19,7 +19,10 @@ const About = () => {
                         I graduated from <a className="text-link" href={raddy} target="_blank" rel="noreferrer">Radford University</a> in 2018 with a Bachelor of Science Degree in Computer Science and Technology, with a concentration in Database Design, as well as an Undergraduate Information Security Certificate recognized by the NSA.
                     </p>
                     <p className="about-info" >
-                        I have a rich educational background in software development, data management, and information security. In addition, I have extensive professional experience implementing those core skills as an IT contractor for the United States Federal Government. As I continued to rapidly take on more responsibility within the company, I consistently demonstrated my ambition and desire for excellence in my work, my aptitude to learn at an accelerated pace, and my enthusiasm to communicate that knowledge to my peers.
+                        I have a rich educational background in software development, data management, and information security. In addition, I have extensive professional experience implementing those core skills as an IT contractor for the United States Federal Government.
+                    </p>
+                    <p>
+                    Through my experience and education, I have always strived to consistently demonstrate my ambition and desire for excellence in my work, my aptitude to learn at an accelerated pace, and my enthusiasm to communicate that knowledge to my peers.
                     </p>
                     <p>
                         At this time, I am eager to specialize further in the field of software development and I am excited to do so in the realm of blockchain technology.
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,34 @@ const Header = () => {
         <div className="component-wraper" id="header">
             <div className="main-info">
                 <br /><br /><br /><br /><br /><br /><br /><br />
-                <h1>Blockchain is the future!</h1>
+                <h1>
+                    <Typed
+                        className="typed-text"
+                        strings={["Blockchain Technology is the future!"]}
+                        typeSpeed={80}
+                        backSpeed={5}
+                        loop
+                    />
+                </h1>
                 <br />
-                <p>My name is Jake Barber, and I am an expierenced Software Developer and IT Proffesional, specializing in blockchain technology.</p>
-                <p>Scroll down to learn more!</p>
-                <div className="typed">
-                <Typed 
-                    className="typed-text"
-                    strings={["Blockchain", "Ethereum", "Solidity", "Web3", "React", "Redux", "Truffle", "Ganache"]}
-                    typeSpeed={80}
-                    backSpeed={100}
-                    loop    
-                    shuffle            
-                />
-                </div>
                 
+                
+                <p>My name is Jake Barber, and I am an expierenced Software Developer and IT Proffesional, specializing in blockchain technology.</p>
+                <p>Scroll down to learn more about me!</p>
+                <div className="typed">
+                    <Typed
+                        className="typed-text"
+                        strings={["Blockchain", "Ethereum", "Solidity", "Web3", "React", "Redux", "Truffle", "Ganache"]}
+                        typeSpeed={80}
+                        backSpeed={100}
+                        loop
+                        shuffle
+                    />
+                </div>
+
 
                 <a href="#contact-info" className="contact"> Contact Me</a>
-            </div>            
+            </div>
         </div>
     )
 }
